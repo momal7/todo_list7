a495ba8a890243525496fb4443a625589c80fc96
momal7 <163012470+momal7@users.noreply.github.com>
2024-04-15 21:29:33 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+
+// creating a cli from "inquirer"
+
+import inquirer from "inquirer";
+
+let todo = []
+
+let condition = true
+
+while (condition) {
+
+    let input = await inquirer.prompt
+    (
+        [
+            {
+                name : "task",
+                message  : "TO DO LIST",
+                type  : "input",
+            },
+
+            {
+                name : "task",
+                message : "DO YOU WANT YO ADD MORE",
+                type : "list",
+                choices : ["yes" , "NO"]
+            }
+        ]
+    );
+
+    todo.push(input.task)
+
+    condition = input.task
+
+}
+console.log(todo);
+
+
